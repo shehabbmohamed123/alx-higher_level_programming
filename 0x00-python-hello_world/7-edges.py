76361827dee963ed0c2061e9bfb26459b690290f
shehabbmohamed123 <shehab12p@gmail.com>
2023-11-27 13:27:50 -0800

    a

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[0:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
+#YOUR CODE GOES HERE.PLESSE REMOVE THIS LINE
 print(f"First 3 letters: {}")
 print(f"Last 2 letters: {}")
 print(f"Middle word: {}")
