0578d290e5c9566d10ffbc1bd5e06340be6dcd09
shehabbmohamed123 <shehab12p@gmail.com>
2024-01-10 12:21:20 -0800

    a

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Add item script."""
 import sys
+import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
